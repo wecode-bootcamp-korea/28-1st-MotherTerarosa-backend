915b96789cc1c18fccb994b92f77c5de73aa1d1b
nine57 <gusrn015@gmail.com>
2021-12-31 12:10:18 +0900

    Modify: user model 수정(point 추가/주소,전화번호 blank 속성 추가)

--- users/models.py
+++ users/models.py
@@ -4,9 +4,10 @@ class User(models.Model):
     name          = models.CharField(max_length=30)
     username      = models.CharField(max_length=30, unique=True)
     password      = models.CharField(max_length=300)
-    address       = models.CharField(max_length=500)
-    mobile_number = models.CharField(max_length=50)
+    address       = models.CharField(max_length=500, blank=True)
+    mobile_number = models.CharField(max_length=50, blank=True)
     email         = models.CharField(max_length=50, unique=True)
+    point         = models.DecimalField(max_digits=9, decimal_places=2, default=1000000)
     created_at    = models.DateTimeField(auto_now_add=True)
     updated_at    = models.DateTimeField(auto_now=True)
 
