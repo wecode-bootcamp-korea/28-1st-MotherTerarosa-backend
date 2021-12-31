59e55932fd3155b361555d4ec77ab3ad516754be
nine57 <gusrn015@gmail.com>
2021-12-31 11:40:44 +0900

    ADD: create initial CartView

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+import json
+from json.decoder import JSONDecodeError
+from os import stat_result
 
-# Create your views here.
+from django.http   import JsonResponse, request
+from django.views  import View
+
+from orders.models   import (
+    Order,
+    Order_Product,
+    OrderStatus
+)
+from products.models import (
+    Menu,
+    Category,
+    Product,
+    TastingNote,
+    Image,
+    ProductStock,
+    Cart
+)
+from users.models    import User
+
+class CartView(View):
+    def post(self, requset):
+
+        try:
+            data = json.loads(request.body)
+
+            if not User.objects.filter(user = data['user']).exists():
+                return JsonResponse({'message':'INVALID_USER'}, status=400)
+
+            carts = Cart.objects.create(
+                quantity = data['quantity'],
+                user     = data['user'],
+                product  = data['product_id'],
+            )
+
+            return JsonResponse({'message': 'CREATED'}, status = 201) 
+
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+        
+        except JSONDecodeError:
+            return JsonResponse({'message': 'JSONDecodeError'}, status=400)
+
+    def get(self, request):
+        pass
